05dca1b35291f6163ec31d72b0953df6d0e3eaf2
IsmailSebz <tsebz07@gmail.com>
2026-06-16 18:08:15 +0300

    Added install button

---
+++ js/sw loader.js
@@ -0,0 +1,10 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(() => {
+            console.log('Service Worker Registered');
+        })
+        .catch(err => {
+            console.error('Service Worker Error:', err);
+        });
+}
